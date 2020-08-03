52de38965493323adb9598af344dbeaec4ce85a0
Christy Varghese <53310662+Christy-Varghese@users.noreply.github.com>
2020-08-03 14:23:47 +0530

    Add files via upload

---
+++ DestroyOutOfBounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }else if(transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!!");
+            Destroy(gameObject);
+        }
+    }
+}
